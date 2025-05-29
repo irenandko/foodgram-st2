7de0e1da73adcad2e7c985869391bf74f52228b2
Ira Lazarenko <lira_2005@bk.ru>
2025-05-29 23:03:10 +0600

    models

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,169 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from users.models import CustomUser
 
-# Create your models here.
+
+class Ingredient(models.Model):
+    """Модель, содержащая информацию о конкретном ингридиенте."""
+
+    name: models.CharField = models.CharField(
+        max_length=128,
+        verbose_name='Название ингредиента')
+    measurement_unit: models.CharField = models.CharField(
+        max_length=32,
+        verbose_name='Единица измерения')
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        ordering = ['name',]
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    """Модель, содержащая информацию о конкретном рецепте."""
+
+    image = models.ImageField(
+        upload_to='recipes_images/',
+        verbose_name='Фото блюда')
+    author: models.ForeignKey = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        verbose_name='Автор рецепта',
+        related_name='recipes',
+    )
+    name: models.CharField = models.CharField(
+        max_length=256,
+        verbose_name='Название рецепта')
+    cooking_time: models.IntegerField = (
+        models.IntegerField(
+            verbose_name='Время приготовления',
+            validators=[
+                MinValueValidator(1),
+            ],
+        )
+    )
+    ingredients: models.ManyToManyField = models.ManyToManyField(
+        Ingredient,
+        through='IngredientInRecipe',
+        related_name='recipes',
+        verbose_name='Ингридиенты'
+    )
+    text: models.TextField = models.TextField(verbose_name='Описание рецепта')
+    pub_date: models.DateTimeField = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации')
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = ['-pub_date',]
+
+    def __str__(self):
+        return self.name
+
+
+class UserRecipeRelation(models.Model):
+    """Модель связи пользователя с рецептом."""
+
+    user: models.ForeignKey = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+    recipe: models.ForeignKey = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт'
+    )
+
+    class Meta:
+        abstract = True
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'recipe',],
+                                    name='%(class)s_unique')
+        ]
+
+    def __str__(self):
+        return f'{self.user} -> {self.recipe}'
+
+
+class IngredientInRecipe(models.Model):
+    """Модель связи ингридиентов и рецептов."""
+
+    recipe: models.ForeignKey = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='ingridients_in_recipe',
+        verbose_name='Рецепт')
+    ingredient: models.ForeignKey = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='ingridients_in_recipe',
+        verbose_name='Ингредиент')
+    amount: models.IntegerField = (
+        models.IntegerField(
+            validators=[MinValueValidator(1),],
+            verbose_name='Количество',
+        )
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент в рецепте'
+        verbose_name_plural = 'Ингредиенты в рецептах'
+
+
+class ShoppingCart(models.Model):
+    """Модель списка покупок (корзины) для пользователя."""
+
+    user: models.ForeignKey = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+        related_name='shopping_carts'
+    )
+    recipe: models.ForeignKey = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт',
+        related_name='in_shopping_carts'
+    )
+
+    class Meta:
+        verbose_name = 'Корзина покупок'
+        verbose_name_plural = 'Корзины покупок'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'],
+                name='unique_shopping_cart'
+            )
+        ]
+
+
+class Favorite(models.Model):
+    """Модель избранных рецептов пользователя."""
+
+    user: models.ForeignKey = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+        related_name='favorites'
+    )
+    recipe: models.ForeignKey = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт',
+        related_name='in_favorites'
+    )
+
+    class Meta:
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранные рецепты'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'recipe'],
+                name='unique_favorite'
+            )
+        ]
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """Класс пользователя."""
+
+    first_name = models.CharField(
+        max_length=128,
+        verbose_name='Имя')
+    last_name = models.CharField(
+        max_length=128,
+        verbose_name='Фамилия',
+    )
+    email = models.EmailField(
+        max_length=128,
+        unique=True,
+        verbose_name='Электронная почта'
+    )
+    username = models.CharField(
+        max_length=50,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=(r'^[\w.@-]+$'),
+                message='Для имени пользователя необходимо использовать'
+                'только буквы, цифры и символы "@ . -"')],
+        verbose_name='Имя пользователя',
+    )
+    avatar = models.ImageField(
+        upload_to='users_avatars/',
+        null=True,
+        blank=True,
+        verbose_name='Фото профиля'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('username',),
+        default_related_name = 'users'
+
+    def __str__(self):
+        return self.username
+
+
+class Subscription(models.Model):
+    """Класс отображения подписки."""
+
+    user: models.ForeignKey = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        verbose_name='Подписчик',
+        related_name='subscribers',
+    )
+    author: models.ForeignKey = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        verbose_name='Автор',
+        related_name='authors'
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки',
+        ordering = ('user',),
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'author',],
+                name='unique_subscription'
+            )
+        ]
